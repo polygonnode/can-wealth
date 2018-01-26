31918206c10977a36b390b463e3ee15fa26c5606
Zachary Metz <zachary.metz@ucalgary.ca>
2018-01-26 13:46:32 -0700

    added survey page

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 
 from flask import Flask                     #import flask module
 from app.views.home import home
+from app.views.planner import planner
 
 def create_app():
     app = Flask(__name__)
@@ -9,7 +10,8 @@ def create_app():
         DEBUG = True,
         SECRET_KEY = 'secret_xxx'
     )
-
+    #register blue prints
     app.register_blueprint(home)
+    app.register_blueprint(planner)
 
     return app
