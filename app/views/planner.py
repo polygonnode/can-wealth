9f262148955408f413a9863651e67dd9ba5f4ac9
pauldan24 <32079319+pauldan24@users.noreply.github.com>
2018-01-31 14:24:15 -0700

    added financial calc

--- app/views/planner.py
+++ app/views/planner.py
@@ -1,6 +1,5 @@
 from flask import request, g, Blueprint, render_template, url_for
 from app.views.forms.planner_forms import Survey_Form
-from app.models
 planner = Blueprint('planner', __name__)
 
 
@@ -8,12 +7,9 @@ planner = Blueprint('planner', __name__)
 def index():
     form = Survey_Form(request.form)
     error = None
-
     if request.method == "POST":
-        print(form.data)
-
         print("survey was posted too reidecting you to result")
-        return url_for('planer.result',id=1234)
+        return url_for('planner.results',id=1234)
     #just return the home page
     return render_template("planner/survey.html",form=form)
 
@@ -21,20 +17,9 @@ def index():
 
 @planner.route("/results")
 def results():
-    survey_id = request.args.get('id')
-    if survey_id != None:
-
-        #find the surevey and run the financeCalculations
-
-        title = "Test Plan"
-        test_goal = 30000
-        test_down_payment = 7000
-        test_monthly_payment = 2500
-        render_template("planner/results.html")
 
 
-
-    return redirect(url_for('planner.index'))
+    return render_template("planner/results.html")
 
 
 
@@ -44,6 +29,30 @@ def financeCalculations(goal, down, monthly):
     remainder = goal - down
     in_account = down
     period = 0
-    while(goal >= monthly): # calculate end of every monthly
-        #end of month
-        in_account = (in_acount * 1.02) +(monthly -5)
+    our_contribution = 0
+    return_list = []
+    while(remainder >= monthly): # calculate end of every monthly
+      #end of month
+      our_contribution = our_contribution + (in_account * 0.0017)
+      account_change = (in_account * 1.0017)  + (monthly -5)
+      remainder = remainder - (account_change - in_account)
+      in_account = account_change
+      period = period + 1
+      print("=========")
+      print(in_account)
+      print(our_contribution)
+      return_list.append([period,in_account,our_contribution])
+      print(return_list)
+    # now the remainder
+    if(remainder > 0):
+      period += 1
+      in_account = in_account + remainder
+      remainder = 0
+
+    print(in_account)
+    print(period)
+    print((our_contribution/in_account)*period)
+
+
+
+    return(return_list)
